f297529ebf0380b674afbd5648f60c9a52a8c857
Asj-Cell <asj701a@gmail.com>
2025-04-15 19:48:25 +0900

    04 14 코드 수정

--- codingTest/project/src/Test4.java
+++ codingTest/project/src/Test4.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Test4 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("입력: ");
-        String input = scanner.nextLine();
-        String result = new StringBuilder(input).reverse().toString();
-        System.out.println("출력: "+result);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("입력: ");
+//        String input = scanner.nextLine();
+//        String result = new StringBuilder(input).reverse().toString();
+//        System.out.println("출력: "+result);
+
     }
 }
--- codingTest/project/src/q0414/q0414.js
+++ codingTest/project/src/q0414/q0414.js
@@ -44,9 +44,9 @@ function isEquals(num){
 
 // 클래스 만들기
 class Wizard{
-     health=0;
-     mana=0;
-     armor=0;
+     let health=0;
+     let mana=0;
+     let armor=0;
 
     constructor(health,mana,armor){
     this.health= health;
