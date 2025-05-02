34f476a47b4a7a7b01d67ff0dafce844c453b325
Asj-Cell <asj701a@gmail.com>
2025-05-02 20:29:57 +0900

    0502 문제수정

--- codingTest/project/src/q0502/Test3.java
+++ codingTest/project/src/q0502/Test3.java
@@ -7,21 +7,26 @@ import java.util.Arrays;
 public class Test3 {
 
     public static void main(String[] args) {
-        String[] result = solution("oxooxoxxox");
+        int[] result = solution("oxooxoxxox");
         System.out.println("result = " + Arrays.toString(result));
     }
 
-    public static String[] solution(String myString) {
-        String[] intArray={};
+    public static int[] solution(String myString) {
+        String[] strArray={};
         int length = 0;
         for (int i = 0; i < myString.length(); i++) {
             if (myString.charAt(i) == 'x') {
                 length++;
             }
-            intArray = myString.split("x", length + 1);
+            strArray = myString.split("x", length + 1);
         }
-        for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = intArray[i].length() + "";
+        for (int i = 0; i < strArray.length; i++) {
+            strArray[i] = strArray[i].length()+"";
+        }
+
+        int[] intArray = new int[strArray.length];
+        for (int i = 0; i < strArray.length; i++) {
+            intArray[i] = Integer.parseInt(strArray[i]);
         }
         return intArray;
     }
