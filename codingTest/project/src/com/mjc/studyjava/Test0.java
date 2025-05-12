8a302f5e8dd0bf3a6e84e7e99d907e32e1dcc483
Asj-Cell <asj701a@gmail.com>
2025-05-12 21:48:05 +0900

    0512 계산기기능수정

--- codingTest/project/src/com/mjc/studyjava/Test0.java
+++ codingTest/project/src/com/mjc/studyjava/Test0.java
@@ -11,16 +11,6 @@ public class Test0 {
         System.out.println(s);
         String s1 = MyMathMathicV3.makeDecryptString("A8dkIm8Tkd4jKLi9hcu4Rsa893saw1kszNm3", 5);
         System.out.println(s1);
-        int total=0;
-        int a = 90;
-        for (int i = a; i <= 100; i++) {
-            total += i;
-        }
-        System.out.println(total);
-
-        BigDecimal bigDecimal = MyMathMathicV3.getBigDecimal(3643, 5630932);
-        System.out.println(bigDecimal);
-        int i =0;
 
     }
 }
