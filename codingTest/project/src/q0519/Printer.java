55158d8363140bfc0b1385439f1149b68d63bbe4
Asj-Cell <158431350+Asj-Cell@users.noreply.github.com>
2025-05-19 21:04:28 +0900

    0519 This is java 챕터6 문제 16

---
+++ codingTest/project/src/q0519/Printer.java
@@ -0,0 +1,18 @@
+package q0519;
+
+public class Printer {
+
+    public static void println(int number) {
+        System.out.println(number);
+    }
+    public static void println(boolean isTrue) {
+        System.out.println(isTrue);
+    }
+
+    public static void println(double number) {
+        System.out.println(number);
+    }
+    public static void println(String str) {
+        System.out.println(str);
+    }
+}
